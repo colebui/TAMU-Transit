72a5a8421c5d22f13441503b731f634ee957dcc5
Emmanuel <emmy0021@tamu.edu>
2020-03-03 13:39:18 -0600

    Added functions to buttons and checkbox for veoride

--- buttons.js
+++ buttons.js
@@ -1,5 +1,7 @@
 var menu = document.getElementById("menu");
 var menuBtn = document.getElementById("menubtn");
+var veorideCheckBox = document.getElementById("veoride");
+var checkboxText = document.getElementById("veorideText")
 function openMenu(){
     menuBtn.style.left = "408px";
     menu.style.left = "0px";
@@ -15,3 +17,31 @@ function closeMenu(){
 
     menuBtn.setAttribute("onClick","openMenu()");
 }
+
+function busButton(){//put all functions for bus button here
+    veorideCheckBox.style.visibility = "hidden";
+    checkboxText.style.visibility = "hidden";
+}
+
+function bikeButton(){//put all functions for bike button here
+    veorideCheckBox.style.visibility = "visible";
+    checkboxText.style.visibility = "visible";
+
+}
+
+function walkButton(){//put all functions for walking button here
+    veorideCheckBox.style.visibility = "hidden";
+    checkboxText.style.visibility = "hidden";
+
+}
+
+function carButton(){//put all functions for car button here
+    veorideCheckBox.style.visibility = "hidden";
+    checkboxText.style.visibility = "hidden";
+}
+
+function wheelchairButton(){//put all functions for wheelchair accessible button here
+    veorideCheckBox.style.visibility = "hidden";
+    checkboxText.style.visibility = "hidden";
+}
+
