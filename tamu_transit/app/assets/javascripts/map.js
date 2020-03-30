1df0df354ff2cc666e09bdced27c2a98795d416e
EC2 Default User <ec2-user@ip-172-31-38-187.us-east-2.compute.internal>
2020-03-30 02:57:56 +0000

    Implemented user current location feature

--- tamu_transit/app/assets/javascripts/map.js
+++ tamu_transit/app/assets/javascripts/map.js
@@ -1,97 +1,145 @@
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    mapTypeControl: false,
-    center: {lat: 30.618811, lng: -96.336424},
-    zoom: 13
-  });
+    var map = new google.maps.Map(document.getElementById('map'), {
+      mapTypeControl: false,
+      center: {lat: 30.618811, lng: -96.336424},
+      zoom: 13
+    });
 
-  new AutocompleteDirectionsHandler(map);
-}
-function AutocompleteDirectionsHandler(map) {
-  this.map = map;
-  this.originPlaceId = null;
-  this.destinationPlaceId = null;
-  this.travelMode = 'WALKING';
-  this.directionsService = new google.maps.DirectionsService;
-  this.directionsRenderer = new google.maps.DirectionsRenderer;
-  this.directionsRenderer.setMap(map);
-  this.directionsRenderer.setPanel(document.getElementById('output'));
+    infoWindow = new google.maps.InfoWindow;
+  
+    new AutocompleteDirectionsHandler(map);
+  }
+  function AutocompleteDirectionsHandler(map) {
+    
+    
+    
 
-  var originInput = document.getElementById('currentLocation');
-  var destinationInput = document.getElementById('destination');
+    this.map = map;
+    this.originPlaceId = null;
+    this.destinationPlaceId = null;
+    this.travelMode = 'WALKING';
+    this.directionsService = new google.maps.DirectionsService;
+    this.directionsRenderer = new google.maps.DirectionsRenderer;
+    var geocoder = new google.maps.Geocoder();
+    this.directionsRenderer.setMap(map);
+    this.directionsRenderer.setPanel(document.getElementById('output'));
 
-  var originAutocomplete = new google.maps.places.Autocomplete(originInput);
-  // Specify just the place data fields that you need.
-  originAutocomplete.setFields(['place_id']);
+    
+  
+    var originInput = document.getElementById('currentLocation');
+    var destinationInput = document.getElementById('destination');
+  
+    var originAutocomplete = new google.maps.places.Autocomplete(originInput);
+    // Specify just the place data fields that you need.
+    originAutocomplete.setFields(['place_id']);
+  
+    var destinationAutocomplete = new google.maps.places.Autocomplete(destinationInput);
+    // Specify just the place data fields that you need.
+    destinationAutocomplete.setFields(['place_id']);
+  
+    this.setupClickListener('walk', 'WALKING');
+    this.setupClickListener('bus', 'TRANSIT');
+    this.setupClickListener('car', 'DRIVING');
+    this.setupClickListener('bike', 'BICYCLING');
+    this.setupClickListener('wheelchair', 'WALKING');
+  
+    this.setupPlaceChangedListener(originAutocomplete, 'ORIG');
+    this.setupPlaceChangedListener(destinationAutocomplete, 'DEST');
 
-  var destinationAutocomplete =
-      new google.maps.places.Autocomplete(destinationInput);
-  // Specify just the place data fields that you need.
-  destinationAutocomplete.setFields(['place_id']);
-
-  this.setupClickListener('walk', 'WALKING');
-  this.setupClickListener('bus', 'TRANSIT');
-  this.setupClickListener('car', 'DRIVING');
-  this.setupClickListener('bike', 'BICYCLING');
-  this.setupClickListener('wheelchair', 'WALKING');
-
-  this.setupPlaceChangedListener(originAutocomplete, 'ORIG');
-  this.setupPlaceChangedListener(destinationAutocomplete, 'DEST');
-
-
-}
-
-// Sets a listener on a radio button to change the filter type on Places
-// Autocomplete.
-AutocompleteDirectionsHandler.prototype.setupClickListener = function(
-    id, mode) {
-  var radioButton = document.getElementById(id);
-  var me = this;
-
-  radioButton.addEventListener('click', function() {
-    me.travelMode = mode;
-    me.route();
-  });
-};
-
-AutocompleteDirectionsHandler.prototype.setupPlaceChangedListener = function(
-    autocomplete, mode) {
-  var me = this;
-  autocomplete.bindTo('bounds', this.map);
-
-  autocomplete.addListener('place_changed', function() {
-    var place = autocomplete.getPlace();
-
-    if (!place.place_id) {
-      window.alert('Please select an option from the dropdown list.');
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        
+        geocoder.geocode({'latLng': latlng}, function(results, status) {
+          if (status == google.maps.GeocoderStatus.OK) {
+            // if the geolocation was recognized and an address was found
+            if (results[0]) {
+              // add a marker to the map on the geolocated point
+              
+              // compose a string with the address parts
+              var address = results[0].address_components[1].long_name+' '+results[0].address_components[0].long_name+', '+results[0].address_components[3].long_name
+              document.getElementById("currentLocation").value = address;
+            }
+          } else {
+            // if the address couldn't be determined, alert and error with the status message
+            alert("Geocoder failed due to: " + status);
+          }
+        });
+        
+        
+      }, function() {
+        handleLocationError(true, infoWindow, map.getCenter());
+      });
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  
+  
+  }
+  
+  // Sets a listener on a radio button to change the filter type on Places
+  // Autocomplete.
+  AutocompleteDirectionsHandler.prototype.setupClickListener = function(
+      id, mode) {
+    var radioButton = document.getElementById(id);
+    var me = this;
+  
+    radioButton.addEventListener('click', function() {
+      me.travelMode = mode;
+      me.route();
+    });
+  };
+  
+  AutocompleteDirectionsHandler.prototype.setupPlaceChangedListener = function(
+      autocomplete, mode) {
+    var me = this;
+    autocomplete.bindTo('bounds', this.map);
+  
+    autocomplete.addListener('place_changed', function() {
+      var place = autocomplete.getPlace();
+  
+      if (!place.place_id) {
+        window.alert('Please select an option from the dropdown list.');
+        return;
+      }
+      if (mode === 'ORIG') {
+        me.originPlaceId = place.place_id;
+      } else {
+        me.destinationPlaceId = place.place_id;
+      }
+      me.route();
+    });
+  };
+  
+  AutocompleteDirectionsHandler.prototype.route = function() {
+    if (!this.originPlaceId || !this.destinationPlaceId) {
       return;
     }
-    if (mode === 'ORIG') {
-      me.originPlaceId = place.place_id;
-    } else {
-      me.destinationPlaceId = place.place_id;
-    }
-    me.route();
-  });
-};
+    var me = this;
 
-AutocompleteDirectionsHandler.prototype.route = function() {
-  if (!this.originPlaceId || !this.destinationPlaceId) {
-    return;
-  }
-  var me = this;
+    
+    
 
-  this.directionsService.route(
-      {
-        origin: {'placeId': this.originPlaceId},
-        destination: {'placeId': this.destinationPlaceId},
-        travelMode: this.travelMode
-      },
-      function(response, status) {
-        if (status === 'OK') {
-          me.directionsRenderer.setDirections(response);
-        } else {
-          window.alert('Directions request failed due to ' + status);
-        }
-      });
-};
+    this.directionsService.route(
+        {
+          origin: {'placeId': this.originPlaceId},
+          destination: {'placeId': this.destinationPlaceId},
+          travelMode: this.travelMode
+        },
+        function(response, status) {
+          if (status === 'OK') {
+            me.directionsRenderer.setDirections(response);
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
+  };
+
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+                          'Error: The Geolocation service failed.' :
+                          'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+  };
