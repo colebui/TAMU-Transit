417fe242627eadc16c4c3714f3b53a73f8e92621
EC2 Default User <ec2-user@ip-172-31-22-241.us-east-2.compute.internal>
2020-04-01 22:13:38 +0000

    updated the ui, made buttons cleaner

--- tamu_transit/app/assets/javascripts/buttons.js
+++ tamu_transit/app/assets/javascripts/buttons.js
@@ -26,47 +26,26 @@ function closeMenu(){
 
 function busButton(){//put all functions for bus button here
     document.getElementById("VeoOption").style.visibility = "visible";
-    document.getElementById("bus").style.color = "Black";
-    document.getElementById("bike").style.color = "#a7a7a7";
-    document.getElementById("walk").style.color = "#a7a7a7";
-    document.getElementById("car").style.color = "#a7a7a7";
-    document.getElementById("wheelchair").style.color = "#a7a7a7";
+
 }
 
 function bikeButton(){//put all functions for bike button here
     document.getElementById("VeoOption").style.visibility = "visible";
-    document.getElementById("bus").style.color = "#a7a7a7";
-    document.getElementById("bike").style.color = "Black";
-    document.getElementById("walk").style.color = "#a7a7a7";
-    document.getElementById("car").style.color = "#a7a7a7";
-    document.getElementById("wheelchair").style.color = "#a7a7a7";
+
 }
 
 function walkButton(){//put all functions for walking button here
     document.getElementById("VeoOption").style.visibility = "hidden";
-    document.getElementById("bus").style.color = "#a7a7a7";
-    document.getElementById("bike").style.color = "#a7a7a7";
-    document.getElementById("walk").style.color = "Black";
-    document.getElementById("car").style.color = "#a7a7a7";
-    document.getElementById("wheelchair").style.color = "#a7a7a7";
+
 }
 
 function carButton(){//put all functions for car button here
     document.getElementById("VeoOption").style.visibility = "hidden";
-    document.getElementById("bus").style.color = "#a7a7a7";
-    document.getElementById("bike").style.color = "#a7a7a7";
-    document.getElementById("walk").style.color = "#a7a7a7";
-    document.getElementById("car").style.color = "Black";
-    document.getElementById("wheelchair").style.color = "#a7a7a7";
+
 }
 
 function wheelchairButton(){//put all functions for wheelchair accessible button here
     document.getElementById("VeoOption").style.visibility = "hidden";
-    document.getElementById("bus").style.color = "#a7a7a7";
-    document.getElementById("bike").style.color = "#a7a7a7";
-    document.getElementById("walk").style.color = "#a7a7a7";
-    document.getElementById("car").style.color = "#a7a7a7";
-    document.getElementById("wheelchair").style.color = "Black";
 }
 
 function firstbox(){
