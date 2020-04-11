c4e4c3cd9cffa0eb00ad0dbf6e5210366ce1387f
eriks <erikswanson@tamu.edu>
2020-04-11 13:14:21 -0500

    Update map.js

--- tamu_transit/app/assets/javascripts/map.js
+++ tamu_transit/app/assets/javascripts/map.js
@@ -98,15 +98,19 @@ function initMap() {
   
     autocomplete.addListener('place_changed', function() {
       var place = autocomplete.getPlace();
-  
+      
       if (!place.place_id) {
         window.alert('Please select an option from the dropdown list.');
         return;
       }
       if (mode === 'ORIG') {
         me.originPlaceId = place.place_id;
+        me.startLat = place.geometry.location.lat();
+        me.startLng = place.geometry.location.lng();
       } else {
         me.destinationPlaceId = place.place_id;
+        me.endLat = place.geometry.location.lat();
+        me.endLng = place.geometry.location.lng();
       }
       me.route();
     });
@@ -120,20 +124,39 @@ function initMap() {
 
     
     
+    if (this.travelMode == 'TRANSIT'){
+      var url = "https://aggiemapsapi.appspot.com/routes?startLat="+this.startLat+"&startLng="+this.startLng+"&endLat="+this.endLat+"&endLng="+this.endLng+"/weekdays";
+      $.ajax({
+        method: "GET",
+        cache: false,
+        url: url,
+        success: function(data) {
+          var path1 = data.path1;
+          var path2 = data.path2;
+          var path3 = data.path3;
 
-    this.directionsService.route(
-        {
-          origin: {'placeId': this.originPlaceId},
-          destination: {'placeId': this.destinationPlaceId},
-          travelMode: this.travelMode
-        },
-        function(response, status) {
-          if (status === 'OK') {
-            me.directionsRenderer.setDirections(response);
-          } else {
-            window.alert('Directions request failed due to ' + status);
-          }
-        });
+          var coords1 = data.coords1;
+          var coords2 = data.coords2;
+          var coords3 = data.coords3;
+          mapit(this.startLat, this.startLng, this.endLat, this.endLng, coords1, path1);
+        }
+      });
+    }
+    else {
+      this.directionsService.route(
+          {
+            origin: {'placeId': this.originPlaceId},
+            destination: {'placeId': this.destinationPlaceId},
+            travelMode: this.travelMode
+          },
+          function(response, status) {
+            if (status === 'OK') {
+              me.directionsRenderer.setDirections(response);
+            } else {
+              window.alert('Directions request failed due to ' + status);
+            }
+          });
+    }
   };
 
   function handleLocationError(browserHasGeolocation, infoWindow, pos) {
@@ -142,4 +165,293 @@ function initMap() {
                           'Error: The Geolocation service failed.' :
                           'Error: Your browser doesn\'t support geolocation.');
     infoWindow.open(map);
+  };
+
+  function mapit(startLat, startLng, endLat, endLng, coords, path) {
+    var waypts = [];
+    for (var i = 0; i < coords.length; i++){
+      waypts.push({
+        location: {lat:coords[i][0],lng:coords[i][1]},
+        stopover: true
+      });
+    }
+    var marker = new google.maps.Marker({
+      map: map,
+    });
+    var walkingPath = {
+      strokeOpacity: 0,
+      icons: [{
+        icon: {
+          path: google.maps.SymbolPath.CIRCLE,
+          fillColor: '#4285F4',
+          fillOpacity: 1,
+          scale: 5,
+        },
+        offset: '5',
+        repeat: '22px'
+      }]
+    };
+    var busPath = {
+      strokeColor: '#06427e',
+      strokeWeight: 7.5
+    };
+    function makeMarker( position, icon ) {
+       new google.maps.Marker({
+        position: position,
+        map: map,
+        icon: icon,
+       });
+     };
+    var circle = {
+       path: google.maps.SymbolPath.CIRCLE,
+       fillColor: 'white',
+       fillOpacity: 1,
+       scale: 5,
+       strokeColor: 'black',
+       strokeWeight: 2.5
+    };
+    var goo = google.maps,
+      map = new goo.Map(document.getElementById('map'), {
+        mapTypeControl: false,
+        streetViewControl: false,
+        fullscreenControl: false,
+        center: {lat:30.6172107, lng: -96.3384577},
+        zoom: 16
+      });
+      App = {
+        map: map,
+        bounds: new goo.LatLngBounds(),
+        directionsService: new goo.DirectionsService(),
+        directionsDisplayStart: new goo.DirectionsRenderer({
+          suppressMarkers: true,
+          map: map,
+          preserveViewport: true,
+          polylineOptions: walkingPath
+        }),
+        directionsDisplayEnd: new goo.DirectionsRenderer({
+          suppressMarkers: true,
+          map: map,
+          preserveViewport: true,
+          polylineOptions: walkingPath
+        }),
+        directionsDisplay1: new goo.DirectionsRenderer({
+          suppressMarkers: true,
+          map: map,
+          preserveViewport: true,
+          polylineOptions: walkingPath
+        }),
+        directionsDisplay2: new goo.DirectionsRenderer({
+          suppressMarkers: true,
+          map: map,
+          preserveViewport: true,
+          polylineOptions: walkingPath
+        }),
+        directionsDisplay3: new goo.DirectionsRenderer({
+          suppressMarkers: true,
+          map: map,
+          preserveViewport: true,
+          polylineOptions: walkingPath
+        }),
+        directionsDisplay4: new goo.DirectionsRenderer({
+          suppressMarkers: true,
+          map: map,
+          preserveViewport: true,
+          polylineOptions: walkingPath
+        }),
+        directionsDisplay5: new goo.DirectionsRenderer({
+          suppressMarkers: true,
+          map: map,
+          preserveViewport: true,
+          polylineOptions: busPath
+        }),
+        directionsDisplay6: new goo.DirectionsRenderer({
+          suppressMarkers: true,
+          map: map,
+          preserveViewport: true,
+          polylineOptions: busPath
+        }),
+        directionsDisplay7: new goo.DirectionsRenderer({
+          suppressMarkers: true,
+          map: map,
+          preserveViewport: true,
+          polylineOptions: busPath
+        }),
+        directionsDisplay8: new goo.DirectionsRenderer({
+          suppressMarkers: true,
+          map: map,
+          preserveViewport: true,
+          polylineOptions: busPath
+        }),
+      },
+
+      App.directionsService.route({
+        origin: {lat:parseFloat(startLat),lng:parseFloat(startLng)},
+        destination: {lat:coords[0][0],lng:coords[0][1]},
+        travelMode: goo.TravelMode.WALKING},
+        function (result, status) {
+          if (status == google.maps.DirectionsStatus.OK) {
+            App.directionsDisplayStart.setDirections(result);
+            var leg = result.routes[ 0 ].legs[ 0 ];
+            makeMarker( leg.start_location, circle );
+            makeMarker( leg.end_location, 'https://raw.githubusercontent.com/danielabreo/aggiemaps/master/data/busicon.PNG' );
+            App.map.fitBounds(App.bounds.union(result.routes[0].bounds));
+          }
+      });
+
+      var boolArray = [true,true,true,true,true,true,true,true];
+      var walking = true;
+
+      for (var i = 0; i < waypts.length-1 ; i++){
+        var j = i+1;
+        var k = (j*2)-1;
+        if (parseInt(path[k])<0){
+          var mode = goo.TravelMode.WALKING;
+          walking = true;
+        } else {
+          var mode = goo.TravelMode.DRIVING;
+          walking = false;
+        }
+        if (boolArray[0] && walking) {
+          boolArray[0] = false;
+          App.directionsService.route({
+            origin: waypts[i].location,
+            destination: waypts[j].location,
+            travelMode: mode
+          },
+          function (result, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+              App.directionsDisplay1.setDirections(result);
+              var leg = result.routes[ 0 ].legs[ 0 ];
+              makeMarker( leg.end_location, 'https://raw.githubusercontent.com/danielabreo/aggiemaps/master/data/busicon.PNG' );
+              App.map.fitBounds(App.bounds.union(result.routes[0].bounds));
+            }
+          });
+        }
+        else if (boolArray[1] && walking) {
+          boolArray[1] = false;
+          App.directionsService.route({
+            origin: waypts[i].location,
+            destination: waypts[j].location,
+            travelMode: mode
+          },
+          function (result, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+              App.directionsDisplay2.setDirections(result);
+              var leg = result.routes[ 0 ].legs[ 0 ];
+              makeMarker( leg.end_location, 'https://raw.githubusercontent.com/danielabreo/aggiemaps/master/data/busicon.PNG' );
+              App.map.fitBounds(App.bounds.union(result.routes[0].bounds));
+            }
+          });
+        }
+        else if (boolArray[2] && walking) {
+          boolArray[2] = false;
+          App.directionsService.route({
+            origin: waypts[i].location,
+            destination: waypts[j].location,
+            travelMode: mode
+          },
+          function (result, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+              App.directionsDisplay3.setDirections(result);
+              var leg = result.routes[ 0 ].legs[ 0 ];
+              makeMarker( leg.end_location, 'https://raw.githubusercontent.com/danielabreo/aggiemaps/master/data/busicon.PNG' );
+              App.map.fitBounds(App.bounds.union(result.routes[0].bounds));
+            }
+          });
+        }
+        else if (boolArray[3] && walking) {
+          boolArray[3] = false;
+          App.directionsService.route({
+            origin: waypts[i].location,
+            destination: waypts[j].location,
+            travelMode: mode
+          },
+          function (result, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+              App.directionsDisplay4.setDirections(result);
+              var leg = result.routes[ 0 ].legs[ 0 ];
+              makeMarker( leg.end_location, 'https://raw.githubusercontent.com/danielabreo/aggiemaps/master/data/busicon.PNG' );
+              App.map.fitBounds(App.bounds.union(result.routes[0].bounds));
+            }
+          });
+        }
+        else if (boolArray[4] && !walking) {
+          boolArray[4] = false;
+          App.directionsService.route({
+            origin: waypts[i].location,
+            destination: waypts[j].location,
+            travelMode: mode
+          },
+          function (result, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+              App.directionsDisplay5.setDirections(result);
+              var leg = result.routes[ 0 ].legs[ 0 ];
+              makeMarker( leg.end_location, 'https://raw.githubusercontent.com/danielabreo/aggiemaps/master/data/busicon.PNG' );
+              App.map.fitBounds(App.bounds.union(result.routes[0].bounds));
+            }
+          });
+        }
+        else if (boolArray[5] && !walking) {
+          boolArray[5] = false;
+          App.directionsService.route({
+            origin: waypts[i].location,
+            destination: waypts[j].location,
+            travelMode: mode
+          },
+          function (result, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+              App.directionsDisplay6.setDirections(result);
+              var leg = result.routes[ 0 ].legs[ 0 ];
+              makeMarker( leg.end_location, 'https://raw.githubusercontent.com/danielabreo/aggiemaps/master/data/busicon.PNG' );
+              App.map.fitBounds(App.bounds.union(result.routes[0].bounds));
+            }
+          });
+        }
+        else if (boolArray[6] && !walking) {
+          boolArray[6] = false;
+          App.directionsService.route({
+            origin: waypts[i].location,
+            destination: waypts[j].location,
+            travelMode: mode
+          },
+          function (result, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+              App.directionsDisplay7.setDirections(result);
+              var leg = result.routes[ 0 ].legs[ 0 ];
+              makeMarker( leg.end_location, 'https://raw.githubusercontent.com/danielabreo/aggiemaps/master/data/busicon.PNG' );
+              App.map.fitBounds(App.bounds.union(result.routes[0].bounds));
+            }
+          });
+        }
+        else if (boolArray[7] && !walking) {
+          boolArray[7] = false;
+          App.directionsService.route({
+            origin: waypts[i].location,
+            destination: waypts[j].location,
+            travelMode: mode
+          },
+          function (result, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+              App.directionsDisplay8.setDirections(result);
+              var leg = result.routes[ 0 ].legs[ 0 ];
+              makeMarker( leg.end_location, 'https://raw.githubusercontent.com/danielabreo/aggiemaps/master/data/busicon.PNG' );
+              App.map.fitBounds(App.bounds.union(result.routes[0].bounds));
+            }
+          });
+        }
+      }
+
+      App.directionsService.route({
+        origin: {lat:coords[coords.length-1][0],lng:coords[coords.length-1][1]},
+        destination: {lat:parseFloat(endLat),lng:parseFloat(endLng)},
+        travelMode: goo.TravelMode.WALKING
+      },
+      function (result, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+          App.directionsDisplayEnd.setDirections(result);
+          App.map.fitBounds(App.bounds.union(result.routes[0].bounds));
+          var leg = result.routes[ 0 ].legs[ 0 ];
+          makeMarker( leg.end_location, marker );
+        }
+      });
   };
